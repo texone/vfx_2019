6e121f9c52d09f46be6cd84aca8d8c18807e73eb
christian riekoff <christian@riekoff.com>
2019-10-17 09:52:19 +0200

    added tree demo

---
+++ Assets/vfx05_tree/Tree.cs
@@ -0,0 +1,218 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using cc.creativecomputing.math.spline;
+using cc.creativecomputing.math.util;
+
+
+
+namespace CambrianExplosion
+{
+    public class TreeSpline
+    {
+        public GameObject gameObject;
+        public CCBezierSpline spline;
+        public float blend = 0;
+
+        public TreeSpline(GameObject theObject, CCBezierSpline theSpline)
+        {
+            gameObject = theObject;
+            spline = theSpline;
+        }
+
+        public bool IsActive()
+        {
+            return gameObject.activeInHierarchy;
+        }
+
+    }
+
+    [ExecuteAlways]
+    public class Tree : MonoBehaviour
+    {
+
+        [SerializeField] private float radius = 0.1f;
+        [SerializeField] private GameObject treeElement = null;
+
+        CCFastRandom random = new CCFastRandom();
+        [SerializeField] int randomSeed = 0;
+
+        public bool drawBezier = true;
+        public bool drawHandles = true;
+
+
+        public int subSplines = 5;
+        public int subDivisions = 1;
+        public int depth = 2;
+
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            // DrawChildrenLines(transform);
+            if (!drawBezier) return;
+
+            _myPaths.ForEach(p => p.spline.Draw(Color.red, 2, drawHandles));
+        }
+
+        private void DrawChildrenLines(Transform t)
+        {
+            Handles.DrawWireDisc(t.position, -SceneView.GetAllSceneCameras()[0].transform.forward, radius);
+
+            for (var i = 0; i < t.childCount; i++)
+            {
+                var childTransform = t.GetChild(i);
+                DrawChildrenLines(childTransform);
+
+                Handles.DrawLine(t.position, childTransform.position);
+            }
+
+        }
+#endif
+
+        [Range(0, 1)]
+        public float curveTensionA = 0.5f;
+        [Range(0, 1)]
+        public float curveTensionB = 0.5f;
+        [Range(0, 10)]
+        public float xRange = 1;
+        [Range(-5, 5)]
+        public float yStart = 2;
+        [Range(0, 10)]
+        public float yRange = 3;
+
+        public int addPoints = 1;
+
+        List<TreeSpline> _myPaths = new List<TreeSpline>();
+
+        private void SubDivideSpline(GameObject theGameObject, CCBezierSpline theSpline, int theDepth) {
+
+            int mySubdivs = random.Random(1, subDivisions);
+            for (int j = 0; j < mySubdivs; j++)
+            {
+                CCBezierSpline myBase = theSpline.Clone();
+                myBase.BeginEditSpline();
+                Vector3 lastPoint = myBase.LastPoint();
+                for (int c = 0; c < addPoints; c++)
+                {
+                    float lastY = lastPoint.y;
+                    lastPoint.x += random.Random(-xRange, xRange);
+                    lastPoint.z += random.Random(-xRange, xRange);
+                    lastPoint.y += random.Random(yStart, yStart + yRange);
+                   // myBase.AddPoint(lastPoint, curveTensionA, curveTensionB);
+                    myBase.AddControlPoints(new Vector3(lastPoint.x, Mathf.Lerp(lastPoint.y, lastY, curveTensionA), lastPoint.z), lastPoint);
+                }
+                myBase.EndEditSpline();
+                if(theDepth > 0)SubDivideSpline(theGameObject, myBase, theDepth - 1);
+                _myPaths.Add(new TreeSpline(theGameObject, myBase));
+            }
+        }
+
+        private CCBezierSpline BuildSpline(Transform theTransform)
+        {
+            CCBezierSpline mySpline = new CCBezierSpline();
+            mySpline.BeginEditSpline();
+            mySpline.AddPoint(theTransform.position);
+            
+            Transform myLastTransform = theTransform;
+            while (myLastTransform.transform.parent != null)
+            {
+                Vector3 a = myLastTransform.position;
+                Vector3 b = myLastTransform.transform.parent.position;
+
+                mySpline.AddControlPoints(
+                    new Vector3(a.x, (a.y + b.y) / 2, a.z),
+                    new Vector3(b.x, (a.y + b.y) / 2, b.z),
+                    b
+                );
+                myLastTransform = myLastTransform.transform.parent;
+
+                
+            }
+            mySpline.EndEditSpline();
+            mySpline.Invert();
+            return mySpline;
+        }
+
+        private void BuildSplines(Transform t)
+        {
+            if (t.childCount == 0)
+            {
+                CCBezierSpline mySpline = BuildSpline(t);
+                _myPaths.Add(new TreeSpline(t.gameObject, mySpline));
+                return;
+            }
+            for (var i = 0; i < t.childCount; i++)
+            {
+                var childTransform = t.GetChild(i);
+                BuildSplines(childTransform);
+            }
+        }
+
+        public bool updateSplines = true;
+
+        [Range(0, 1)]
+        public float endRandomMin = 0.3f;
+        [Range(0, 1)]
+        public float endRandomMax = 1f;
+
+        private void UpdateSplines()
+        {
+            //if (_myPaths != null && _myPaths.Count > 0) return;
+
+            Debug.Log("Update Splines");
+
+            _myPaths.Clear();
+
+            BuildSplines(transform);
+
+            random.RandomSeed(randomSeed);
+
+            new List<TreeSpline>(_myPaths).ForEach(p => {
+                int mySubSplines = random.Random(1, subSplines);
+                for (int i = 0; i < subSplines; i++)
+                {
+                    CCBezierSpline s = p.spline.SubSpline(0, random.Random(endRandomMin, endRandomMax)); ;
+                    SubDivideSpline(p.gameObject, s, depth);
+                }
+            });
+        }
+
+
+        [Range(0, 10)]
+        public float blendTime = 1f;
+
+        private void OnValidate()
+        {
+            UpdateSplines();
+        }
+
+
+        private void Update()
+        {
+            if(updateSplines) UpdateSplines();
+
+            _myPaths.ForEach(p => {
+                if (p.IsActive())
+                {
+                    p.blend += Time.deltaTime / blendTime;
+                }
+                else
+                {
+                    p.blend -= Time.deltaTime / blendTime;
+                }
+                p.blend = Mathf.Clamp01(p.blend);
+               // Debug.Log(p.blend);
+            });
+        }
+
+        public List<TreeSpline> Paths()
+        {
+            return _myPaths;
+        }
+
+
+
+    }
+}
---
+++ Assets/vfx05_tree/TreeBaker.cs
@@ -0,0 +1,219 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using cc.creativecomputing.math.spline;
+
+namespace CambrianExplosion
+{
+
+    
+    [ExecuteAlways]
+    public class TreeBaker : MonoBehaviour
+    {
+        #region Editable attributes
+
+        [SerializeField] RenderTexture _positionMap = null;
+        [SerializeField] RenderTexture _attributeMap = null;
+        [SerializeField] ComputeShader _compute = null;
+
+        [SerializeField] float branchXZ = 0.5f;
+        [SerializeField] float branchYMin = 0.5f;
+        [SerializeField] float branchYMax = 1f;
+
+        #endregion
+
+        #region Temporary objects
+
+        int[] _dimensions = new int[2];
+
+        List<Vector3> _positionList = new List<Vector3>();
+        List<Vector3> _attributeList = new List<Vector3>();
+
+        ComputeBuffer _positionBuffer;
+        ComputeBuffer _attributeBuffer;
+
+        RenderTexture _tempPositionMap;
+        RenderTexture _tempAttributeMap;
+
+        #endregion
+
+        #region Private methods
+
+        private static RenderTexture CreateRenderTexture(int width, int height)
+        {
+            var rt = new RenderTexture(width, height, 0, RenderTextureFormat.ARGBHalf);
+            rt.enableRandomWrite = true;
+            rt.Create();
+            return rt;
+        }
+
+        private void TransferData(int theNumberOfSplines)
+        {
+            var mapWidth = _positionMap.width;
+            var mapHeight = _positionMap.height;
+
+            var vcount = _positionList.Count;
+            var vcount_x3 = vcount * 3;
+
+            // Release the temporary objects when the size of them don't match
+            // the input.
+
+            if (_positionBuffer != null && _positionBuffer.count != vcount_x3)
+            {
+                _positionBuffer.Dispose();
+                _attributeBuffer.Dispose();
+
+                _positionBuffer = null;
+                _attributeBuffer = null;
+            }
+
+            if (_tempPositionMap != null &&
+               (_tempPositionMap.width != mapWidth ||
+                _tempPositionMap.height != mapHeight))
+            {
+                Destroy(_tempPositionMap);
+                Destroy(_tempAttributeMap);
+
+                _tempPositionMap = null;
+                _tempAttributeMap = null;
+            }
+
+            // Lazy initialization of temporary objects
+
+            if (_positionBuffer == null)
+            {
+                _positionBuffer = new ComputeBuffer(vcount_x3, sizeof(float));
+                _attributeBuffer = new ComputeBuffer(vcount_x3, sizeof(float));
+            }
+
+            if (_tempPositionMap == null)
+            {
+                _tempPositionMap = CreateRenderTexture(mapWidth, mapHeight);
+                _tempAttributeMap = CreateRenderTexture(mapWidth, mapHeight);
+            }
+
+            // Set data and execute the transfer task.
+            _compute.SetInt("VertexCount", mapWidth);
+            _compute.SetInt("SplineCount", theNumberOfSplines);
+            _compute.SetMatrix("Transform", transform.localToWorldMatrix);
+
+            _positionBuffer.SetData(_positionList);
+            _attributeBuffer.SetData(_attributeList);
+
+            _compute.SetBuffer(0, "PositionBuffer", _positionBuffer);
+            _compute.SetBuffer(0, "AttributeBuffer", _attributeBuffer);
+
+            _compute.SetTexture(0, "PositionMap", _tempPositionMap);
+            _compute.SetTexture(0, "AttributeMap", _tempAttributeMap);
+
+            _compute.Dispatch(0, mapWidth / 8, mapHeight / 8, 1);
+
+            Graphics.CopyTexture(_tempPositionMap, _positionMap);
+            Graphics.CopyTexture(_tempAttributeMap, _attributeMap);
+        }
+
+        bool _warned;
+
+        bool CheckConsistency()
+        {
+            if (_warned) return false;
+
+            if (_positionMap.width % 8 != 0 || _positionMap.height % 8 != 0)
+            {
+                Debug.LogError("Position map dimensions should be a multiple of 8.");
+                _warned = true;
+            }
+
+            if (_attributeMap.width != _positionMap.width ||
+               _attributeMap.height != _positionMap.height)
+            {
+                Debug.LogError("Position/normal map dimensions should match.");
+                _warned = true;
+            }
+
+            if (_positionMap.format != RenderTextureFormat.ARGBHalf &&
+               _positionMap.format != RenderTextureFormat.ARGBFloat)
+            {
+                Debug.LogError("Position map format should be ARGBHalf or ARGBFloat.");
+                _warned = true;
+            }
+
+            if (_attributeMap.format != RenderTextureFormat.ARGBHalf &&
+               _attributeMap.format != RenderTextureFormat.ARGBFloat)
+            {
+                Debug.LogError("Normal map format should be ARGBHalf or ARGBFloat.");
+                _warned = true;
+            }
+
+            return !_warned;
+        }
+
+        
+
+        #endregion
+
+
+
+        #region MonoBehaviour implementation
+        // Start is called before the first frame update
+        void Start() { }
+
+        public bool modActive = false;
+        // Update is called once per frame
+        void Update()
+        {
+            Random.InitState(255);
+            _positionList.Clear();
+            _attributeList.Clear();
+
+            Tree myTree = GetComponent<Tree>();
+            List<TreeSpline> mySplines = myTree.Paths();
+
+            for (int y = 0; y < mySplines.Count;y++)
+            {
+                TreeSpline mySpline = mySplines[y];
+                List<Vector3> Vertices = mySpline.spline.Discretize(_positionMap.width);
+                for (int x = 0; x < _positionMap.width;x++)
+                {
+                    _positionList.Add(Vertices[x]);
+                    _attributeList.Add(new Vector3(mySpline.IsActive() ? 1 : 0, mySpline.blend, 0));
+                }
+            }
+
+            if (!CheckConsistency()) return;
+            
+            TransferData(mySplines.Count);
+        }
+
+        public static void TryDispose(System.IDisposable obj)
+        {
+            if (obj == null) return;
+            obj.Dispose();
+        }
+
+        public static void TryDestroy(UnityEngine.Object obj)
+        {
+            if (obj == null) return;
+            UnityEngine.Object.Destroy(obj);
+        }
+
+        void OnDestroy()
+        {
+
+            TryDispose(_positionBuffer);
+            TryDispose(_attributeBuffer);
+
+            TryDestroy(_tempPositionMap);
+            TryDestroy(_tempAttributeMap);
+
+            _positionBuffer = null;
+            _attributeBuffer = null;
+
+            _tempPositionMap = null;
+            _tempAttributeMap = null;
+        }
+
+        #endregion
+    }
+}
